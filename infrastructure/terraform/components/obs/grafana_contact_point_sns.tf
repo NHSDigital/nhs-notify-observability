388387d65819084b631e49e01e0331aafd270d86
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-02-14 16:05:05 +0000

    CCM-8201: Grafana Alarming Infra

--- infrastructure/terraform/components/obs/grafana_contact_point_sns.tf
+++ infrastructure/terraform/components/obs/grafana_contact_point_sns.tf
@@ -2,8 +2,9 @@ resource "grafana_contact_point" "sns" {
   name = "${local.csi}-alerting-sns"
 
   sns {
-    topic                   = aws_sns_topic.alerting.arn
-    assume_role_arn         = aws_iam_role.sns_alert_role.arn
-    message_format          = json
+    topic           = aws_sns_topic.alerting.arn
+    assume_role_arn = aws_iam_role.sns_alert_role.arn
+    message_format  = "json"
+    subject         = "{{ template \"default.title\" .}}"
   }
 }
