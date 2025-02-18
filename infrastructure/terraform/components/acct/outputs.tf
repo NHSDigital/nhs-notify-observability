7c61cfb65da769b77f53a0406b93073399220aaf
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-02-18 13:34:05 +0000

    CCM-8201: Add Lambda Artefact Bucket

--- infrastructure/terraform/components/acct/outputs.tf
+++ infrastructure/terraform/components/acct/outputs.tf
@@ -5,3 +5,13 @@ output "dns_zone" {
     nameservers = aws_route53_zone.main.name_servers
   }
 }
+
+output "s3_buckets" {
+  value = {
+    lambda_function_artefacts = {
+      arn    = module.s3bucket_lambda_artefacts.arn
+      bucket = module.s3bucket_lambda_artefacts.bucket
+      id     = module.s3bucket_lambda_artefacts.id
+    }
+  }
+}
