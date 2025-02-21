fd2a573bdd1659cda56d2244b10c46b3f7246c7e
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-02-21 15:05:51 +0000

    CCM-8201: Grafana Alarming Infra

--- infrastructure/terraform/components/obs/locals_reporting_rules.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  reporting_rule_files = [
-    "s3_backup_failures",
-    "completed_batch_report_executions_aborted",
-    "completed_batch_report_executions_failed",
-    "completed_batch_report_executions_timed_out"
-  ]
-}
