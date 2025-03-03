231036dbc16c73e389e82a91937cd3607c97c643
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-03-03 10:13:45 +0000

    CCM-8201: Grafana Alarming Infra

--- infrastructure/terraform/components/obs/data_iam_policy_document_sns_alerting_assume_role.tf
+++ infrastructure/terraform/components/obs/data_iam_policy_document_sns_alerting_assume_role.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "sns_alerting_assume_role" {
     effect = "Allow"
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${var.aws_account_id}:role/nhs-notify-${var.environment}-acct-grafana-workspace-role"]
+      identifiers = ["arn:aws:iam::${var.aws_account_id}:role/nhs-notify-${var.environment}-grafana-workspace-role"]
     }
     actions = ["sts:AssumeRole"]
   }
--- infrastructure/terraform/components/obs/grafana_rule_group_reporting.tf
+++ infrastructure/terraform/components/obs/grafana_rule_group_reporting.tf
@@ -337,4 +337,115 @@ resource "grafana_rule_group" "reporting" {
       mute_timings  = null
     }
   }
+rule {
+    name      = "S3 Backup Failures"
+    condition = "C"
+
+    data {
+      ref_id = "A"
+
+      relative_time_range {
+        from = 600
+        to   = 0
+      }
+
+      datasource_uid = grafana_data_source.cloudwatch_cross_account["reporting"].uid
+      model          = jsonencode({
+        datasource = {
+          type = "cloudwatch"
+          uid  = grafana_data_source.cloudwatch_cross_account["reporting"].uid
+        }
+        expression       = ""
+        id               = ""
+        intervalMs       = 1000
+        label            = ""
+        logGroups        = []
+        matchExact       = true
+        maxDataPoints    = 43200
+        metricEditorMode = 0
+        metricName       = "NumberOfBackupJobsFailed"
+        metricQueryType  = 0
+        namespace        = "AWS/Backup"
+        period           = "60"
+        queryMode        = "Metrics"
+        refId            = "A"
+        region           = "default"
+        sqlExpression    = ""
+        statistic        = "Sum"
+      })
+    }
+
+    data {
+      ref_id = "B"
+
+      relative_time_range {
+        from = 600
+        to   = 0
+      }
+
+      datasource_uid = "__expr__"
+      model          = jsonencode({
+        conditions = [{
+          evaluator = { params = [1], type = "gt" }
+          operator  = { type = "and" }
+          query     = { params = ["A"] }
+          reducer   = { params = [], type = "last" }
+          type      = "query"
+        }]
+        datasource = {
+          type = "__expr__"
+          uid  = "__expr__"
+        }
+        expression     = "A"
+        intervalMs     = 1000
+        maxDataPoints  = 43200
+        reducer        = "last"
+        refId          = "B"
+        settings       = { mode = "" }
+        type           = "reduce"
+      })
+    }
+
+    data {
+      ref_id = "C"
+
+      relative_time_range {
+        from = 600
+        to   = 0
+      }
+
+      datasource_uid = "__expr__"
+      model          = jsonencode({
+        conditions = [{
+          evaluator = { params = [0], type = "gt" }
+          operator  = { type = "and" }
+          query     = { params = ["B"] }
+          reducer   = { params = [], type = "last" }
+          type      = "query"
+        }]
+        datasource = {
+          type = "__expr__"
+          uid  = "__expr__"
+        }
+        expression    = "B"
+        intervalMs    = 1000
+        maxDataPoints = 43200
+        refId         = "C"
+        type          = "threshold"
+      })
+    }
+
+    no_data_state  = "NoData"
+    exec_err_state = "Error"
+    for            = "1h"
+    annotations    = {}
+    labels         = {}
+    is_paused      = false
+
+    notification_settings {
+      contact_point = grafana_contact_point.sns.name
+      group_by      = null
+      mute_timings  = null
+    }
+  }
 }
