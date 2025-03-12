b8ccb92f29a40a5beb44d560b3497db2190e5a90
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-03-12 09:57:40 +0000

    CCM-8589: Events Alarming Infra

--- infrastructure/terraform/components/obs/cloudwatch_event_target_main.tf
+++ infrastructure/terraform/components/obs/cloudwatch_event_target_main.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_target" "main" {
   rule           = aws_cloudwatch_event_rule.main.name
-  event_bus_name = aws_cloudwatch_event_bus.main.name
+  event_bus_name = data.terraform_remote_state.acct.outputs.event_bus_name
   arn            = module.lambda_alert_forwarding.function_arn
 }
