ce87337a115b04aaa6cfbbd1fc12e1b8932d863f
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-05-07 16:38:25 +0100

    CCM-8197: Firehose Splunk Delivery

--- infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
+++ infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
@@ -20,6 +20,7 @@ data "aws_iam_policy_document" "github_deploy" {
       "grafana:*",
       "identitystore:Get*",
       "oam:*",
+      "ram:*",
       "sns:*",
       "sso:AssociateProfile",
       "sso:DisassociateProfile",
