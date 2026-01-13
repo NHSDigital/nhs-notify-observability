53261ab789004f6142db9bb85583e60717ea42d6
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-01-13 11:47:07 +0000

    CCM-13883: Trivy Optimisation

--- infrastructure/terraform/components/obsconfig/variables.tf
+++ infrastructure/terraform/components/obsconfig/variables.tf
@@ -82,6 +82,6 @@ variable "bounded_context_account_ids" {
 
 variable "parent_acct_environment" {
   type        = string
-  description = "Name of the environment responsible for the acct resources used, affects things like DNS zone. Useful for named dev environments test"
+  description = "Name of the environment responsible for the acct resources used, affects things like DNS zone. Useful for named dev environments"
   default     = "main"
 }
